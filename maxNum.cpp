434037ce6110ae63d1102904b82d53582eb6ddcf
leavesYeah <49064785+leavesYeah@users.noreply.github.com>
2020-01-21 23:38:57 +0800

    Create maxNum.cpp

---
+++ maxNum.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int MaxNum(int *a,int n){
+    int b=0,maxN=0;
+    for(int i=0;i<n;i++){
+        if(b<=0)
+            b=a[i];
+        else
+            b+=a[i];
+
+        if(b>maxN){
+            maxN=b;
+        }
+    }
+    return maxN;
+}
+int main()
+{
+    int n;
+    cin>>n;
+    int a[n];
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+    int m=MaxNum(a,n);
+    cout<<m;
+    return 0;
+}
